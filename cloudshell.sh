88eec4d6a108ee673eddaf48b6276b2dae6fda3c
Samsul Hadi <samsulhadi@Samsuls-MacBook-Air-2.local>
2021-03-13 10:22:42 +0700

    reduce input

--- cloudshell.sh
+++ cloudshell.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# git clone https://github.com/GoogleCloudPlatform/terraform-google-lb
-cd terraform-google-lb/examples/basic
+cd ~/
+
+git clone https://github.com/GoogleCloudPlatform/terraform-google-lb
+cd ~/terraform-google-lb/examples/basic
 
 export GOOGLE_PROJECT=$(gcloud config get-value project)
 
 terraform init
-terraform plan
-terraform apply
+terraform plan -out=tfplan -var "project_id=$GOOGLE_PROJECT"
+terraform apply tfplan
 
 EXTERNAL_IP=$(terraform output | grep load_balancer_default_ip | cut -d = -f2 | xargs echo -n)
 echo "http://${EXTERNAL_IP}"
